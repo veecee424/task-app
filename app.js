85e85775b6066ef2e7da9221841ff7d6fcec690c
veecee <valentine424@yahoo.com>
2020-06-08 12:39:54 +0100

    Resolved server issue between localhost and heroku

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
-/** 
- * Express setup
-*/
+//Express setup
 const express = require('express')
 const app = express();
 app.use(express.json());
 
-/** 
- * Require database connection
-*/
+//Require database connection
 require('./db/mongoose')
 
-/** 
- * Body-parser setup
-*/
+//Setup body-parser
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-/**
- * Service maintenance middleware
- */
-// app.use((req, res, next) => {
-//     res.status(503).send('Maintenance ongoing, check back later')
-//  })
+
 app.get('/', (req, res) => {
     return res.send('Welcome to my task manager API!')
 });
 
-/** 
- * Use routes
-*/
+//Use routes
 const userRoutes = require('./router/userRoutes')
 app.use(userRoutes)
 const taskRoutes = require('./router/taskRoutes');
